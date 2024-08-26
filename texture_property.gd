ca82906bb333d67e3f18a847ca12fb993f7c885b
Shuflduf <shuflduf@gmail.com>
2024-08-26 15:07:44 -0600

    setting textures

--- texture_property.gd
+++ texture_property.gd
@@ -3,13 +3,26 @@ extends HBoxContainer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	for i in DirAccess.get_files_at("res://assets/textures/"):
+		if i.ends_with(".import"):
+			continue
+		print(i)
+		var new_texture = TextureRect.new()
+		new_texture.texture = ResourceLoader.load("res://assets/textures/" + i)
+		new_texture.name = i + "T"
+		new_texture.expand_mode = TextureRect.EXPAND_IGNORE_SIZE
+		new_texture.custom_minimum_size = Vector2(100, 100)
+		new_texture.gui_input.connect(_on_new_texture_rect_gui_input.bind(i))
+		$TextureSelection/Textures.add_child(new_texture)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
+func _on_new_texture_rect_gui_input(event: InputEvent, texture_name: String) -> void:
+	if event is InputEventMouseButton and event.is_pressed():
+		if event.button_mask == MOUSE_BUTTON_MASK_LEFT:
+			$TextureRect.texture = ResourceLoader.load("res://assets/textures/" + texture_name)
+			$"../../..".active_material.albedo_texture = $TextureRect.texture
 
 func _on_texture_rect_gui_input(event: InputEvent) -> void:
-	pass # Replace with function body.
+	if event is InputEventMouseButton and event.is_pressed():
+		if event.button_mask == MOUSE_BUTTON_MASK_LEFT:
+			$TextureSelection.position = event.global_position
+			$TextureSelection.show()
