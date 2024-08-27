f969f50ec9e6c9a55f3901490904f7a1fe8f5f1d
Shuflduf <shuflduf@gmail.com>
2024-08-26 20:08:46 -0600

    box preview

--- materials.gd
+++ materials.gd
@@ -10,8 +10,7 @@ func _ready() -> void:
 	active_material = platform_material
 
 func _process(delta: float) -> void:
-	$VBoxContainer/HBoxContainer2/SubViewportContainer/SubViewport/MeshInstance3D\
-			.rotation.y += delta * 0.2
+	%Sphere.rotation.y += delta * 0.2
 
 
 func _on_tab_bar_tab_changed(tab: int) -> void:
@@ -23,3 +22,16 @@ func _on_tab_bar_tab_changed(tab: int) -> void:
 
 	for i in $VBoxContainer/HBoxContainer2/VBoxContainer.get_children():
 		i.update_values()
+	%Sphere.set_surface_override_material(0, active_material)
+
+
+func _on_mesh_type_tab_changed(tab: int) -> void:
+	match tab:
+		0:
+			%Sphere.mesh = SphereMesh.new()
+			%Sphere.mesh.radial_segments = 128
+			%Sphere.mesh.radial_segments = 64
+			%Sphere.scale = Vector3(1, 1, 1)
+		1:
+			%Sphere.mesh = BoxMesh.new()
+			%Sphere.scale = Vector3(0.7, 0.7, 0.7)
