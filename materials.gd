a96721e2e656cfe45fd3dfe0fdfe73b048deaa71
Shuflduf <shuflduf@gmail.com>
2024-08-26 21:11:28 -0600

    reset to defaults

--- materials.gd
+++ materials.gd
@@ -3,6 +3,7 @@ extends MarginContainer
 
 @export var platform_material: StandardMaterial3D
 @export var box_material: StandardMaterial3D
+@export var default_material: StandardMaterial3D
 
 var active_material: StandardMaterial3D
 
@@ -21,7 +22,8 @@ func _on_tab_bar_tab_changed(tab: int) -> void:
 			active_material = box_material
 
 	for i in $VBoxContainer/HBoxContainer2/VBoxContainer.get_children():
-		i.update_values()
+		if i.has_method("update_values"):
+			i.update_values()
 	%Sphere.set_surface_override_material(0, active_material)
 
 
@@ -35,3 +37,18 @@ func _on_mesh_type_tab_changed(tab: int) -> void:
 		1:
 			%Sphere.mesh = BoxMesh.new()
 			%Sphere.scale = Vector3(0.7, 0.7, 0.7)
+
+
+func _on_button_pressed() -> void:
+	#match $VBoxContainer/HBoxContainer2/VBoxContainer2/MeshType.current_tab:
+		#0:
+			#platform_material = default_material.duplicate()
+		#1:
+			#box_material = default_material.duplicate()
+	for i in default_material.get_property_list():
+		print(i)
+		active_material.set(i["name"], default_material.get(i["name"]))
+
+	for i in $VBoxContainer/HBoxContainer2/VBoxContainer.get_children():
+		if i.has_method("update_values"):
+			i.update_values()
